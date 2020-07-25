6ca796ba8704278a67efdf758d02619e253cfad6
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2020-07-25 17:53:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7c36b3e090629e56e2547df86c92cd6.js"
+  "/precache-manifest.2b94e0181bac641689efc964b430fbfd.js"
 );
 
 self.addEventListener('message', (event) => {

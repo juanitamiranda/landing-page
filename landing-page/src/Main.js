d08817bca560def4d5e49d7e1b8a823afb4ff2df
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2020-07-26 19:42:36 -0400

    Updating

--- landing-page/src/Main.js
+++ landing-page/src/Main.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 class Main extends Component {
   render() {
     return (
-      <div className='main'>
-        <h1>Juanita Miranda Ortiz</h1>
-        <h2>Agente de Seguros</h2>
-        <h3>Planes Médicos Individuales y Comerciales | Seguros de Vida</h3>
-        <h4>Información de Contacto</h4>
-        <p>Teléfono: <a href="tel:+17874870474">+1 (787) 487-0474</a></p>
-        <p>Correo Electrónico: <a href="mailto:juani_986@yahoo.com">juani_986@yahoo.com</a></p>
+      <div className="main">
+      	<div className="hero">
+      		<h1>Juanita Miranda Ortiz</h1>
+       		<h2>Agente de Seguros</h2>
+      	</div>
+        <div className="contact">
+          <h4>Información de Contacto</h4>
+          <p>Teléfono: <a href="tel:+17874870474">+1 (787) 487-0474</a></p>
+          <p>Correo Electrónico: <a href="mailto:juani_986@yahoo.com">juani_986@yahoo.com</a></p>
+        </div>
       </div>
     );
   }

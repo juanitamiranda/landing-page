43e2e5f641c6368b87428a1f7e388ba8bb1be5d7
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2020-08-12 23:19:06 -0400

    Updating

--- landing-page/src/Main.js
+++ landing-page/src/Main.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import Juanita from "./images/juanitamirandaortiz.png";
 import Phone from "./images/phone.png";
 import Email from "./images/email.png";
+import Beneficios from "./images/beneficios.png";
+import BeneficiosPDF from "./pdf/beneficios.pdf";
+import Salus from "./images/salus.png";
+import SalusPDF from "./pdf/salus.pdf";
+import Cancer from "./images/cancer.png";
+import CancerPDF from "./pdf/cancer.pdf";
+import Dental from "./images/dental.png";
+import DentalPDF from "./pdf/dental.pdf";
+
 
 class Main extends Component {
   constructor(){
@@ -12,7 +21,9 @@ class Main extends Component {
       job: "Agente de Seguros", 
       slogan: "Estar asegurado es estar protedigo.",
       services: ["Planes Médicos", "Cáncer", "Seguros de Vida"],
-      contact: ["Foto de Juanita Miranda Ortiz", "INFORMACIÓN DE CONTACTO", "Número de Teléfono", "Correo Electrónico"]
+      download: "Descargue:",
+      flyers: ["Red de Clínicas", "Póliza de Cáncer", "Dental", "Resumen de Beneficios"],
+      contact: ["Foto de Juanita Miranda Ortiz", "Información de Contacto", "Número de Teléfono", "Correo Electrónico"]
     }
     this.es = this.es.bind(this);
     this.en = this.en.bind(this);
@@ -28,7 +39,9 @@ class Main extends Component {
       job: "Agente de Seguros", 
       slogan: "Estar asegurado es estar protedigo.",
       services: ["Planes Médicos", "Cáncer", "Seguros de Vida"],
-      contact: ["Foto de Juanita Miranda Ortiz", "INFORMACIÓN DE CONTACTO", "Número de Teléfono", "Correo Electrónico"]
+      download: "Descargue:",
+      flyers: ["Red de Clínicas", "Póliza de Cáncer", "Dental", "Resumen de Beneficios"],
+      contact: ["Foto de Juanita Miranda Ortiz", "Información de Contacto", "Número de Teléfono", "Correo Electrónico"]
     });
   }
 
@@ -38,7 +51,9 @@ class Main extends Component {
       job: "Insurance Agent", 
       slogan: "To be insured is to be protected.",
       services: ["Healthcare Plan", "Cancer", "Life Insurance"],
-      contact: ["Photo of Juanita Miranda Ortiz", "CONTACT INFORMATION", "Phone Number", "Email Address"]
+      download: "Download:",
+      flyers: ["Clinics", "Cancer Insurance", "Dental", "Summary of Benefits"],
+      contact: ["Photo of Juanita Miranda Ortiz", "Contact Information", "Phone Number", "Email Address"]
     });
   }
 
@@ -53,25 +68,51 @@ class Main extends Component {
       	<div className="hero">
       		<h1>Juanita Miranda Ortiz</h1>
        		<h2>{this.state.job}</h2>
-          <h3>{this.state.slogan}</h3>
       	</div>
-        <div className="services">
-          <div className="flex">
-          <h3>{this.state.services[0]}</h3>
-          </div>
-          <div className="flex">
-          <h3>{this.state.services[1]}</h3>
-          </div>
-          <div className="flex">
-          <h3>{this.state.services[2]}</h3>
+        <div className="slogan">
+          <div className="slogan-flex">
+          <h3>{this.state.slogan}</h3>
           </div>
         </div>
-        <div className="container">
+        <div className="services">
+          <div className="services-flex">
+          <h4>{this.state.services[0]}</h4>
+          </div>
+          <div className="services-flex">
+          <h4>{this.state.services[1]}</h4>
+          </div>
+          <div className="services-flex">
+          <h4>{this.state.services[2]}</h4>
+          </div>
+        </div>
+        <div class="line"></div>
+        <div className="download">
+        {this.state.download}
+        </div>
+        <div className="flyers">
+          <div className="flyers-flex">
+          <a href={SalusPDF} download="FOLLETO RED DE CLINICAS - TE CUIDAMOS DONDEQUIERA 2020.pdf" target="_black"><img src={Salus} alt="{this.state.flyers[0]}" /></a>
+          <p>{this.state.flyers[0]}</p>
+          </div>
+          <div className="flyers-flex">
+          <a href={CancerPDF} download="Flyer Poliza Protect-Digital.pdf" target="_black"><img src={Cancer} alt={this.state.flyers[1]} /></a>
+          <p>{this.state.flyers[1]}</p>
+          </div>
+          <div className="flyers-flex">
+          <a href={DentalPDF} download="FOLLETO DENTAL - 2020.pdf" target="_black"><img src={Dental} alt={this.state.flyers[2]} /></a>
+          <p>{this.state.flyers[2]}</p>
+          </div>
+          <div className="flyers-flex">
+          <a href={BeneficiosPDF} download="FOLLETO RESUMEN BENEFICIOS 2020 digitalactualizado 2.pdf" target="_black"><img src={Beneficios} alt={this.state.flyers[3]} /></a>
+          <p>{this.state.flyers[3]}</p>
+          </div>
+        </div>
+        <div className="info">
           <div className="photo">
             <img className="agentphoto" src={Juanita} alt={this.state.contact[0]} />
           </div>
           <div className="contact">
-            <h4>{this.state.contact[1]}</h4>
+            <h5>{this.state.contact[1]}</h5>
             <p><img className="icon" src={Phone} alt={this.state.contact[2]} /> <a href="tel:+17874870474">+1 (787) 487-0474</a></p>
             <p><img className="icon" src={Email} alt={this.state.contact[3]} /> <a href="mailto:juani_986@yahoo.com">juani_986@yahoo.com</a></p>
           </div>

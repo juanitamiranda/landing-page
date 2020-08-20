2c768bdcc3505998202e5bf7203ded2720a2dcfc
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2020-08-20 07:42:33 -0400

    Updating

--- landing-page/src/Main.js
+++ landing-page/src/Main.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Juanita from "./images/juanitamirandaortiz.png";
 import Phone from "./images/phone.png";
 import Email from "./images/email.png";
+import TripleS1 from "./images/triples1.png";
+import TripleS2 from "./images/triples2.png";
+import TripleS3 from "./images/triples3.jpg";
 import Beneficios from "./images/beneficios.png";
 import BeneficiosPDF from "./pdf/beneficios.pdf";
 import Salus from "./images/salus.png";
@@ -22,8 +25,9 @@ class Main extends Component {
     this.state = {
       title: "Juanita Miranda Ortiz | Agente de Seguros",
       job: "Agente de Seguros", 
-      slogan: "Estar asegurado es estar protedigo.",
-      services: ["Planes Médicos", "Cáncer", "Seguros de Vida"],
+      triples: ["Precaver te ayuda a protegerte a ti y a tu familia del cáncer y 30 enfermedades temidas.", "Triple-S Vida: Cuando más lo necesiten.", "Triple-S Directo: El plan de salud completo que te ofrece realizar consultas médicas desde tu teléfono o computadora con TELECONSULTA MD.", "Comunícate conmigo para orientarte sobre el plan de salud más completo. Triple-S Directo te ofrece una red con más de 13,000 proveedores de salud y te brinda el innovador servicio TeleConsulta MD, que te permite consultar con un médico desde tu celular o computadora dondequiera que estés. Llámame al (787) 487-0474 para más información."],
+      slogan: "Estar Cubiertos es estar Seguros.",
+      services: ["Recibe orientación sobre:", "Planes Médicos", "Seguro de Cáncer", "Seguros de Vida"],
       download: "Descargue:",
       flyers: ["Red de Clínicas", "Folleto de la Póliza de Cáncer", "Folleto de la Cubierta Dental", "Resumen de Beneficios"],
       contact: ["Foto de Juanita Miranda Ortiz", "Información de Contacto", "Número de Teléfono", "Correo Electrónico"]
@@ -40,8 +44,9 @@ class Main extends Component {
     this.setState({
       title: "Juanita Miranda Ortiz | Agente de Seguros",
       job: "Agente de Seguros", 
-      slogan: "Estar asegurado es estar protedigo.",
-      services: ["Planes Médicos", "Cáncer", "Seguros de Vida"],
+      triples: ["Precaver te ayuda a protegerte a ti y a tu familia del cáncer y 30 enfermedades temidas.", "Triple-S Vida: Cuando más lo necesiten.", "Triple-S Directo: El plan de salud completo que te ofrece realizar consultas médicas desde tu teléfono o computadora con TELECONSULTA MD.", "Comunícate conmigo para orientarte sobre el plan de salud más completo. Triple-S Directo te ofrece una red con más de 13,000 proveedores de salud y te brinda el innovador servicio TeleConsulta MD, que te permite consultar con un médico desde tu celular o computadora dondequiera que estés. Llámame al (787) 487-0474 para más información."],
+      slogan: "Estar Cubiertos es estar Seguros.",
+      services: ["Recibe orientación sobre:", "Planes Médicos", "Seguro de Cáncer", "Seguros de Vida"],
       download: "Descargue:",
       flyers: ["Red de Clínicas", "Folleto de la Póliza de Cáncer", "Folleto de la Cubierta Dental", "Resumen de Beneficios"],
       contact: ["Foto de Juanita Miranda Ortiz", "Información de Contacto", "Número de Teléfono", "Correo Electrónico"]
@@ -52,8 +57,9 @@ class Main extends Component {
     this.setState({
       title: "Juanita Miranda Ortiz | Insurance Agent",
       job: "Insurance Agent", 
-      slogan: "To be insured is to be protected.",
-      services: ["Healthcare Plan", "Cancer", "Life Insurance"],
+      triples: ['Prevention helps protect you and your family from cancer and 30 dreaded diseases.', 'Triple-S Vida: When you need it most.', 'Triple-S Directo: The health plan that offers medical consultations from your phone or computer with TELECONSULTA MD.', 'Contact me to receive orientation on the most complete health plan. Triple-S Directo offers you a network with more than 13,000 health providers and offers you the innovative TeleConsulta MD service, which allows you to consult with a doctor from your cell phone or computer wherever you are. Call me at (787) 487-0474 for more information.'],
+      slogan: "Being Covered is being Safe.",
+      services: ["Receive orientation about:", "Healthcare Plan", "Cancer Insurance", "Life Insurance"],
       download: "Download:",
       flyers: ["Clinics", "Cancer Insurance Brochure", "Dental Coverage Brochure", "Summary of Benefits"],
       contact: ["Photo of Juanita Miranda Ortiz", "Contact Information", "Phone Number", "Email Address"]
@@ -68,27 +74,31 @@ class Main extends Component {
           <span className="language"> | </span>
           <button onClick={this.es}>Español</button>
         </div>
-      	<div className="hero">
-      		<h1>Juanita Miranda Ortiz</h1>
-       		<h2>{this.state.job}</h2>
-      	</div>
         <div className="slogan">
-          <div className="slogan-flex">
+        <div className="slogan-flex">
           <h3>{this.state.slogan}</h3>
-          </div>
         </div>
+        </div>
+      	<div className="hero">
+          <img className="triples1" src={TripleS1} alt={this.state.triples[0]} title={this.state.triples[0]} />
+          <img className="triples2" src={TripleS2} alt={this.state.triples[1]} title={this.state.triples[1]} />
+          <video loop autoPlay className="video" title={this.state.triples[2]} >
+          <source src={require('../src/videos/triples.mp4')} type="video/mp4" />
+          </video>
+          <img className="triples3" src={TripleS3} alt={this.state.triples[3]} title={this.state.triples[3]} />
+      	</div>
         <div className="services">
           <div className="services-flex">
-          <h4>{this.state.services[0]}</h4>
-          <img src={PlanesMedicos} alt={this.state.services[0]} />
-          </div>
-          <div className="services-flex">
           <h4>{this.state.services[1]}</h4>
-          <img src={CancerRibbon} alt={this.state.services[1]} />
+          <img src={PlanesMedicos} alt={this.state.services[1]} title={this.state.services[1]} />
           </div>
           <div className="services-flex">
           <h4>{this.state.services[2]}</h4>
-          <img src={Vida} alt={this.state.services[2]} />
+          <img src={CancerRibbon} alt={this.state.services[2]} title={this.state.services[2]} />
+          </div>
+          <div className="services-flex">
+          <h4>{this.state.services[3]}</h4>
+          <img src={Vida} alt={this.state.services[3]} title={this.state.services[3]} />
           </div>
         </div>
         <div class="line"></div>
@@ -97,30 +107,31 @@ class Main extends Component {
         </div>
         <div className="flyers">
           <div className="flyers-flex">
-          <a href={SalusPDF} download="FOLLETO RED DE CLINICAS - TE CUIDAMOS DONDEQUIERA 2020.pdf" target="_black"><img src={Salus} alt="{this.state.flyers[0]}" /></a>
-          <p><a href={SalusPDF} download="FOLLETO RED DE CLINICAS - TE CUIDAMOS DONDEQUIERA 2020.pdf" target="_black">{this.state.flyers[0]}</a></p>
+            <a href={SalusPDF} download="FOLLETO RED DE CLINICAS - TE CUIDAMOS DONDEQUIERA 2020.pdf" target="_blank"><img src={Salus} alt={this.state.flyers[0]} title={this.state.flyers[0]} /></a>
+            <p><a href={SalusPDF} download="FOLLETO RED DE CLINICAS - TE CUIDAMOS DONDEQUIERA 2020.pdf" target="_blank">{this.state.flyers[0]}</a></p>
           </div>
           <div className="flyers-flex">
-          <a href={CancerPDF} download="Flyer Poliza Protect-Digital.pdf" target="_black"><img src={Cancer} alt={this.state.flyers[1]} /></a>
-          <p><a href={CancerPDF} download="Flyer Poliza Protect-Digital.pdf" target="_black">{this.state.flyers[1]}</a></p>
+            <a href={CancerPDF} download="Flyer Poliza Protect-Digital.pdf" target="_blank"><img src={Cancer} alt={this.state.flyers[1]} title={this.state.flyers[1]} /></a>
+            <p><a href={CancerPDF} download="Flyer Poliza Protect-Digital.pdf" target="_blank">{this.state.flyers[1]}</a></p>
           </div>
           <div className="flyers-flex">
-          <a href={DentalPDF} download="FOLLETO DENTAL - 2020.pdf" target="_black"><img src={Dental} alt={this.state.flyers[2]} /></a>
-          <p><a href={DentalPDF} download="FOLLETO DENTAL - 2020.pdf" target="_black">{this.state.flyers[2]}</a></p>
+            <a href={DentalPDF} download="FOLLETO DENTAL - 2020.pdf" target="_blank"><img src={Dental} alt={this.state.flyers[2]} title={this.state.flyers[2]} /></a>
+            <p><a href={DentalPDF} download="FOLLETO DENTAL - 2020.pdf" target="_blank">{this.state.flyers[2]}</a></p>
           </div>
           <div className="flyers-flex">
-          <a href={BeneficiosPDF} download="FOLLETO RESUMEN BENEFICIOS 2020 digitalactualizado 2.pdf" target="_black"><img src={Beneficios} alt={this.state.flyers[3]} /></a>
-          <p><a href={BeneficiosPDF} download="FOLLETO RESUMEN BENEFICIOS 2020 digitalactualizado 2.pdf" target="_black">{this.state.flyers[3]}</a></p>
+            <a href={BeneficiosPDF} download="FOLLETO RESUMEN BENEFICIOS 2020 digitalactualizado 2.pdf" target="_blank"><img src={Beneficios} alt={this.state.flyers[3]} title={this.state.flyers[3]} /></a>
+            <p><a href={BeneficiosPDF} download="FOLLETO RESUMEN BENEFICIOS 2020 digitalactualizado 2.pdf" target="_blank">{this.state.flyers[3]}</a></p>
           </div>
         </div>
         <div className="info">
           <div className="photo">
-            <img className="agentphoto" src={Juanita} alt={this.state.contact[0]} />
+            <img className="agentphoto" src={Juanita} alt={this.state.contact[0]} title={this.state.contact[0]} />
           </div>
           <div className="contact">
-            <h5>{this.state.contact[1]}</h5>
-            <p><img className="icon" src={Phone} alt={this.state.contact[2]} /> <a href="tel:+17874870474">+1 (787) 487-0474</a></p>
-            <p><img className="icon" src={Email} alt={this.state.contact[3]} /> <a href="mailto:juani_986@yahoo.com">juani_986@yahoo.com</a></p>
+            <h5>Juanita Miranda Ortiz</h5>
+            <h6>{this.state.job}</h6>
+            <p><img className="icon" src={Phone} alt={this.state.contact[2]} /> <a href="tel:+17874870474" title={this.state.contact[2]} >+1 (787) 487-0474</a></p>
+            <p><img className="icon" src={Email} alt={this.state.contact[3]} /> <a href="mailto:juani_986@yahoo.com" title={this.state.contact[3]} >juani_986@yahoo.com</a></p>
           </div>
         </div>
       </div>

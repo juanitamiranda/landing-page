74dfbcecf61693ec240860829ab9b1c0b39d0785
Josue Perez <che.perez3@gmail.com>
2021-06-09 17:43:32 -0400

    deploy missing files

---
+++ precache-manifest.f926c28078c145e6d1a37b8d8d094e66.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694dbf6465ef57e66cd7ccaa6089d0ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6f4eb28c0145a0f547",
+    "url": "/static/css/main.0c210ff1.chunk.css"
+  },
+  {
+    "revision": "70774dee61534a3ac538",
+    "url": "/static/js/2.f48d8385.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f48d8385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6f4eb28c0145a0f547",
+    "url": "/static/js/main.41c00653.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  },
+  {
+    "revision": "8767b0145ffc99174ceb0991092742d7",
+    "url": "/static/media/Brochure-Dondequiera.8767b014.pdf"
+  },
+  {
+    "revision": "77f17522355fd428949fc49ed05610e2",
+    "url": "/static/media/Endoso-Dental_DI10-2021.77f17522.pdf"
+  },
+  {
+    "revision": "9f17d79f9d1b262370ef367b0325455b",
+    "url": "/static/media/FOLLETO RED DE CLINICAS - TE CUIDAMOS DONDEQUIERA 2020.9f17d79f.pdf"
+  },
+  {
+    "revision": "69deb074c04c402d64cd519e33f8f42c",
+    "url": "/static/media/Flyer-Poliza_Protect-Digital.69deb074.pdf"
+  },
+  {
+    "revision": "5c1dffe96f3b4676ea67a3047ffff400",
+    "url": "/static/media/banner.5c1dffe9.png"
+  },
+  {
+    "revision": "ab3efd7ddd870ba761d6074cd5f0bc2e",
+    "url": "/static/media/cancer.ab3efd7d.png"
+  },
+  {
+    "revision": "c71b3484d225765ef2efb512f55a3da6",
+    "url": "/static/media/cancerribbon.c71b3484.png"
+  },
+  {
+    "revision": "d4e35297dc1ec4a8b8408f0255385a32",
+    "url": "/static/media/dental.d4e35297.png"
+  },
+  {
+    "revision": "b55078d5e3405d8cfd06a0975f280e8c",
+    "url": "/static/media/email.b55078d5.png"
+  },
+  {
+    "revision": "525e0fa4347662e0496156110b75ff27",
+    "url": "/static/media/hero.525e0fa4.jpg"
+  },
+  {
+    "revision": "352908378d3e2647d6046843b4b78fe3",
+    "url": "/static/media/juanitamirandaortiz.35290837.png"
+  },
+  {
+    "revision": "8d1e2feeeea705d28c8b88b16fe82eb6",
+    "url": "/static/media/planesmedicos.8d1e2fee.png"
+  },
+  {
+    "revision": "a1f8db6f94657b0847c59fea4853101d",
+    "url": "/static/media/resumen-beneficio-21.a1f8db6f.png"
+  },
+  {
+    "revision": "20eedba9acd4ed190c6af405809d103b",
+    "url": "/static/media/salus.20eedba9.png"
+  },
+  {
+    "revision": "ba48e1812a65fb2ee39d590a5d61dcba",
+    "url": "/static/media/triples1.ba48e181.png"
+  },
+  {
+    "revision": "188a5f77c1b0472948dcfd6d278eba19",
+    "url": "/static/media/triples2.188a5f77.png"
+  },
+  {
+    "revision": "6add6637c2909007bfef2c4e775df61f",
+    "url": "/static/media/triples3.6add6637.jpg"
+  },
+  {
+    "revision": "abea22df13487b51b2f8f1bcd5e1de5f",
+    "url": "/static/media/vida.abea22df.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.789ae71d35fdf70957e17577806f9e82.js"
+  "/precache-manifest.f926c28078c145e6d1a37b8d8d094e66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

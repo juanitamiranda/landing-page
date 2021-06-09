bfe7c6978591deb9502bbcbe6647cc17afc37560
che-perez <che.perez3@gmail.com>
2021-06-09 17:11:38 -0400

    remove outaded images

--- landing-page/src/Main.js
+++ landing-page/src/Main.js
@@ -6,7 +6,7 @@ import TripleS3 from "./images/triples3.jpg";
 import PlanesMedicos from "./images/planesmedicos.png";
 import CancerRibbon from "./images/cancerribbon.png";
 import Vida from "./images/vida.png";
-import Beneficios from "./images/beneficios.png";
+import Beneficios from "./images/resumen-beneficio-21.png";
 import Salus from "./images/salus.png";
 import Cancer from "./images/cancer.png";
 import Dental from "./images/dental.png";
@@ -24,10 +24,6 @@ class Main extends Component {
           	<article>
               <img src={TripleS1} alt={this.props.state.triples[0]} title={this.props.state.triples[0]} />
               <img src={TripleS2} alt={this.props.state.triples[1]} title={this.props.state.triples[1]} />
-              <video autoplay="false" className="border" title={this.props.state.triples[2]} >
-              <source src={require('../src/videos/triples.mp4')} type="video/mp4" />
-              </video>             
-              <img className="border" src={TripleS3} alt={this.props.state.triples[3]} title={this.props.state.triples[3]} />
             </article>
             <section className="services">
               <div>
@@ -49,20 +45,20 @@ class Main extends Component {
             </section>
             <section className="pdf">
                 <div>
-                  <a href={SalusPDF} target="_blank" rel="noopener noreferrer"><img src={Salus} alt={this.props.state.pdf[0]} title={this.props.state.pdf[0]} /></a>
-                  <p><a href={SalusPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[0]}</a></p>
-                </div>
-                <div>
-                  <a href={CancerPDF} target="_blank" rel="noopener noreferrer"><img src={Cancer} alt={this.props.state.pdf[1]} title={this.props.state.pdf[1]} /></a>
-                  <p><a href={CancerPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[1]}</a></p>
+                  <a href={BeneficiosPDF} target="_blank" rel="noopener noreferrer"><img src={Beneficios} alt={this.props.state.pdf[3]} title={this.props.state.pdf[3]} /></a>
+                  <p><a href={BeneficiosPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[3]}</a></p>
                 </div>
                 <div>
                   <a href={DentalPDF} target="_blank" rel="noopener noreferrer"><img src={Dental} alt={this.props.state.pdf[2]} title={this.props.state.pdf[2]} /></a>
                   <p><a href={DentalPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[2]}</a></p>
                 </div>
                 <div>
-                  <a href={BeneficiosPDF} target="_blank" rel="noopener noreferrer"><img src={Beneficios} alt={this.props.state.pdf[3]} title={this.props.state.pdf[3]} /></a>
-                  <p><a href={BeneficiosPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[3]}</a></p>
+                  <a href={SalusPDF} target="_blank" rel="noopener noreferrer"><img src={Salus} alt={this.props.state.pdf[0]} title={this.props.state.pdf[0]} /></a>
+                  <p><a href={SalusPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[0]}</a></p>
+                </div>
+                <div>
+                  <a href={CancerPDF} target="_blank" rel="noopener noreferrer"><img src={Cancer} alt={this.props.state.pdf[1]} title={this.props.state.pdf[1]} /></a>
+                  <p><a href={CancerPDF} target="_blank" rel="noopener noreferrer">{this.props.state.pdf[1]}</a></p>
                 </div>
             </section>
         </div>
